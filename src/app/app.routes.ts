286d6723fb48c3064c589fada3d35340ec841269
Mauricio Lenis <mauricio.lenisb@pragma.com.co>
2026-05-12 16:45:41 -0500

    feat: lint configurated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,30 +1,29 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  
-  
   {
     path: 'base',
-    loadComponent: () => import('./presentation/pages/base/base.page').then( m => m.BasePage),
+    loadComponent: () => import('./presentation/pages/base/base.page').then(m => m.BasePage),
     children: [
       {
         path: 'categories',
-        loadComponent: () => import('./presentation/pages/categories/categories.page').then( m => m.CategoriesPage)
+        loadComponent: () =>
+          import('./presentation/pages/categories/categories.page').then(m => m.CategoriesPage),
       },
       {
         path: 'tasks',
-        loadComponent: () => import('./presentation/pages/tasks/tasks.page').then( m => m.TasksPage)
+        loadComponent: () => import('./presentation/pages/tasks/tasks.page').then(m => m.TasksPage),
       },
       {
         path: '**',
         pathMatch: 'full',
-        redirectTo: 'base/tasks'
-      }
-    ]
+        redirectTo: 'base/tasks',
+      },
+    ],
   },
   {
     path: '**',
     pathMatch: 'full',
-    redirectTo: 'base/tasks'
+    redirectTo: 'base/tasks',
   },
 ];
